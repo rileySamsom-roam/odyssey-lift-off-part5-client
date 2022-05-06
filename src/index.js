5697236650f1fa7cc5eba482cc74213457f883d8
riley-samsom <69017910+riley-samsom@users.noreply.github.com>
2022-05-06 14:00:16 +1200

    update to prod server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://server-my-graph-2000.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
